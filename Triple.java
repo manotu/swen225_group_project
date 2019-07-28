c674600dd4b422755f7b36d4ebf8f9236cd564ef
louruiz <53197263+louruiz@users.noreply.github.com>
2019-07-28 22:35:11 +1200

    Add files via upload

---
+++ Triple.java
@@ -0,0 +1,12 @@
+public class Triple {
+    Card weapon;
+    Card room;
+    Card character;
+
+    public Triple(Card weapon, Card room, Card character) {
+        this.weapon = weapon;
+        this.room = room;
+        this.character = character;
+    }
+
+}
